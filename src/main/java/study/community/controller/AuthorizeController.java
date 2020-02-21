f7eb5f026e3c0a2709c3b823d3bb81f94518bcb7
= <=>
2020-02-21 21:58:41 +0800

    User's document autowired by application.properties

--- src/main/java/study/community/controller/AuthorizeController.java
+++ src/main/java/study/community/controller/AuthorizeController.java
@@ -1,6 +1,7 @@
 package study.community.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,15 +15,17 @@ public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
 
+    @Value("${github.client.id}")
+    String client_id;
+    @Value("${github.client.secret}")
+    String client_secret;
+    @Value("${github.redirect.uri}")
+    String redirect_uri;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state) {
-        AccessTokenDTO accessTokenDTO = new AccessTokenDTO(
-                "Iv1.994bc79e4a21d0c5",
-                "a8859f51af261047566ff5bb84e56c127e327cbf",
-                code,
-                "http://localhost:8080/callback",
-                state);
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO(client_id,client_secret,code,redirect_uri,state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser githubUser = githubProvider.getUesr(accessToken);
         return "index";
