1fe95d4dfa9d0dbf6b7c8e6cf3bcab641e3d800f
= <=>
2020-03-15 23:33:41 +0800

    add request I

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 
 ## [学习视频](https://www.bilibili.com/video/av65117012)
 
-##m mybatis generator maven启动指令 
+## mybatis generator maven启动指令 
     mvn -Dmybatis.generator.overwrite=true mybatis-generator:generate
---
+++ src/main/java/study/community/controller/CommentController.java
@@ -0,0 +1,37 @@
+package study.community.controller;
+
+import com.sun.deploy.net.HttpRequest;
+import com.sun.deploy.net.HttpResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import study.community.dto.CommentDTO;
+import study.community.mapper.CommentMapper;
+import study.community.model.Comment;
+
+@Controller
+public class CommentController {
+
+    @SuppressWarnings("all")
+    @Autowired
+    private CommentMapper commentMapper;
+
+    @ResponseBody
+    @RequestMapping(value = "/comment",method = RequestMethod.POST)
+    public Object post(@RequestBody CommentDTO commentDTO,
+                       HttpRequest request){
+        Comment comment = new Comment();
+        comment.setParentId(commentDTO.getParentId());
+        comment.setContent(commentDTO.getContent());
+        comment.setType(commentDTO.getType());
+        comment.setGmtCreate(System.currentTimeMillis());
+        comment.setGmtModified(comment.getGmtCreate());
+        comment.setCommentator(1);
+        comment.setLikeCount(0L);
+        commentMapper.insert(comment);
+        return null;
+    }
+}
